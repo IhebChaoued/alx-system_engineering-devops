66fa256d5710c5ba070e15bbacb23b899e59cee3
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-19 22:53:56 +0000

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,18 @@
 #!/usr/bin/pup
 # Puppet: install Flask using pip3
 
-package { 'Flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
-require  => Package['python3-pip'],
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
