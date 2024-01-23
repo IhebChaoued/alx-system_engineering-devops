c18aba05930d2afef963b0229c824896b37550d2
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-23 19:08:35 +0000

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,27 +3,18 @@ package { 'nginx':
   ensure => installed,
 }
 
-file { '/etc/nginx/sites-available/redirect_me':
-  ensure  => file,
-  content => "server {
-    listen 80;
-
-    server_name _;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}",
-  notify => Service['nginx'],
+file_line { 'NotDefault':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  ensure  => file,
   content => 'Hello World!',
-  notify  => Service['nginx'],
 }
 
 service { 'nginx':
   ensure => running,
-  enable => true,
+  require => Package['nginx'],
 }
