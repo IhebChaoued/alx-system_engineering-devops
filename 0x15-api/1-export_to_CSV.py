10e0826a9b7d23db555929c7b5527412b3fabb2b
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-19 22:08:42 +0000

    CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """ Return informations using REST Api """
-
 import csv
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    user_id = argv[1]
 
-def get_user_data(user_id):
-    user_link = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    return requests.get(user_link).json()
-
-
-def get_todo_data(user_id):
     todo_link = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-    return requests.get(todo_link).json()
+    user_link = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
+    user_resp = requests.get(user_link)
+    todo_resp = requests.get(todo_link)
 
-def export_to_csv(user_id, user_data, todo_data):
+    user_data = user_resp.json()
+    todo_data = todo_resp.json()
+
     user = user_data["username"]
-    file_name = f"{user_id}.csv"
 
+    file_name = f"{user_id}.csv"
     with open(file_name, mode="w", newline='') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
@@ -27,17 +26,3 @@ def export_to_csv(user_id, user_data, todo_data):
             writer.writerow(
                     [user_id, user, todo_item['completed'], todo_item['title']]
                     )
-
-    print(f"Data has been exported to {file_name}")
-
-
-def main():
-    user_id = argv[1]
-    user_data = get_user_data(user_id)
-    todo_data = get_todo_data(user_id)
-
-    export_to_csv(user_id, user_data, todo_data)
-
-
-if __name__ == "__main__":
-    main()
